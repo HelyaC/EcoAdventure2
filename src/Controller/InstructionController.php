d433767bda3ed8496fa85bdd991e5f44719ef8f1
Aality <aality@MacBook-Pro-de-Aality.local>
2024-11-07 17:24:55 +0100

    page cours dynamique

--- src/Controller/InstructionController.php
+++ src/Controller/InstructionController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\CoursRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class InstructionController extends AbstractController
 {
     #[Route('/instruction', name: 'app_instruction')]
-    public function index(): Response
+    public function index(CoursRepository $coursRepository): Response
     {
+        // Récupère tous les cours de la base de données
+        $cours = $coursRepository->findAll();
+
+        // Passe les cours à la vue
         return $this->render('instruction/index.html.twig', [
-            'controller_name' => 'InstructionController',
+            'cours' => $cours,
         ]);
     }
 }
